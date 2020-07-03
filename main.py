56d6c9dfd239cc026db58f7f4c75012056014c1e
Stephen Hull <stm.hull@gmail.com>
2020-07-03 16:14:59 -0500

    barebones discord command configured

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python3
+
 from google.cloud import storage
 import sqlite3
+from flask import Flask
+import os
+import discord
+
 
 # get db from cloud storage
 storage_client = storage.Client()
@@ -10,10 +16,10 @@ db = conn.cursor()
 
 # save to cloud storage
 def save_db():
-  conn.commit()
-  storage.Blob('wanparty.db', bucket).upload_from_filename('/tmp/wanparty.db')
-  db.close()
-  conn.close()
+		conn.commit()
+		storage.Blob('wanparty.db', bucket).upload_from_filename('/tmp/wanparty.db')
+		db.close()
+		conn.close()
 
 db.execute('SELECT * FROM counts;')
 rows = db.fetchall()
@@ -26,3 +32,15 @@ save_db()
 def discord_bot(request):
   return "hello world, someday I will be a discord bot with persistent data and a ci/cd pipeline"
 
+# Bot biz
+app = Flask(__name__)
+client = discord.Client()
+@app.route('/')
+def hello_world():
+		return "Hello, world!"
+@client.event
+async def on_ready():
+		print(f'Logged in as {client.user}')
+# TODO token should change to be whatever it needs to be in the cloud
+token = os.environ['DISCORD_SECRET']
+client.run(token)
