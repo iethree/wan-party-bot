8ecdaa3941610d7861efa6d3f8b9c91be81f5f3e
Tim Macdonald <tsmacdonald@gmail.com>
2021-02-28 14:22:01 -0600

    What is OOP?

--- message_handler.py
+++ message_handler.py
@@ -1,34 +1,30 @@
 from discord.utils import get
 import random
-
-def get_emoji(guild, emojiName):
-  try:
-    return get(guild.emojis, name=emojiName)
-  except Exception as e:
-    return '🙃'
+from reaction import *
 
 def sometimes(chance):
   return random.random() < chance
 
+STATIC_REACTIONS = [
+    Reaction('poop', '💩'),
+    Reaction(['DRG', 'dwarf'], ['🪨', '🥌']), # rock and stone
+    Reaction('ps5', '👎'),
+    MatchingReaction(lambda c, m: sometimes(0.1) and 'shplay' in m.author.display_name.lower(), '🙄')
+]
+
+def get_emoji(guild, emoji_name):
+  try:
+    return get(guild.emojis, name=emoji_name)
+  except Exception as e:
+    return '🙃'
+
 async def respond_to(message):
   content = message.content.lower()
-
-  if 'the way' in content:
-    await message.add_reaction(get_emoji(message.guild, 'mando'))
-
-  if 'poop' in content:
-    await message.add_reaction('💩')
-
-  if 'shplay' in message.author.display_name.lower():
-    sometimes(0.1) and await message.add_reaction('🙄')
-
-  if 'DRG' in content or 'dwarf' in content:
-    await message.add_reaction('🪨') # rock
-    await message.add_reaction('🥌') # stone
-
-  if 'ps5' in content:
-    await message.add_reaction('👎')
-
-  if 'star wars' in content and message.channel.name != 'star-wars':
-    await message.add_reaction(get_emoji(message.guild, 'stormtrooper'))
+  reactions = STATIC_REACTIONS + [
+      Reaction('the way', get_emoji(message.guild, 'mando')),
+      MatchingReaction(lambda c, m: 'star wars' in c and m.channel.name != 'star-wars', get_emoji(message.guild, 'stormtrooper'))
+  ]
+  for reaction in reactions:
+      if reaction.matches(content, message):
+          await reaction.apply_to(message)
 
---
+++ reaction.py
@@ -0,0 +1,24 @@
+def listify(maybe_list):
+    maybe_list if isinstance(maybe_list, type([])) else [maybe_list]
+
+class Reaction:
+    def __init__(self, keywords, reactions):
+        self.keywords = listify(keywords)
+        self.reactions = listify(reactions)
+
+    def matches(self, content, message=None):
+        return any(kw in content for kw in self.keywords)
+
+    def apply_to(self, message):
+        for reaction in self.reactions:
+            print("Adding " + reaction + "!")
+            message.add_reaction(reaction)
+
+class MatchingReaction(Reaction):
+    def __init__(self, matcher, reactions):
+        self.matcher = matcher
+        super().__init__(None, reactions)
+
+    def matches(self, content, message=None):
+        return self.matcher(content, message)
+
