ead76863fbd60b5b79ab813c91702dc4efaf3581
Benjamin Kietzman <bengilgit@gmail.com>
2021-02-28 07:40:51 -0500

    Update messageHandler.py

--- messageHandler.py
+++ messageHandler.py
@@ -1,8 +1,11 @@
+from discord.utils import get
+
+mando = get(ctx.message.server.emojis, name="mando")
 
 def respondToMessage(message):
   name = (message.author.nick or message.author.name or message.author)
   content = message.content.lower()
   if 'the way' in content:
-    return ':mando:'
+    return mando
   if 'poop' in content:
     return ":poop:"
