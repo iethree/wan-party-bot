cc13314c5cd3da72d9923ee2edc2fb09a72d8946
Ryan Laurie <iethree@gmail.com>
2021-03-01 06:31:47 -0600

    see me rollin

--- message_handler.py
+++ message_handler.py
@@ -13,6 +13,8 @@ STATIC_REACTIONS = [
     MatchingReaction(lambda c, m: sometimes(0.1) and 'shplay' in m.author.display_name.lower(), '🙄')
 ]
 
+
+
 def get_emoji(guild, emoji_name):
   try:
     return get(guild.emojis, name=emoji_name)
@@ -24,12 +26,23 @@ async def respond_to(message):
       res = sub.run(message.content.split()[1:],
           stdout=sub.PIPE, stderr=sub.PIPE)
       return (res.stdout + res.stderr).decode('utf-8')
+
   content = message.content.lower()
+
   reactions = STATIC_REACTIONS + [
       Reaction('the way', get_emoji(message.guild, 'mando')),
-      MatchingReaction(lambda c, m: 'star wars' in c and m.channel.name != 'star-wars', get_emoji(message.guild, 'stormtrooper'))
+      MatchingReaction(lambda c, m: 'star wars' in c and m.channel.name != 'star-wars', get_emoji(message.guild, 'stormtrooper')),
   ]
+  
   for reaction in reactions:
       if reaction.matches(content, message):
           await reaction.apply_to(message)
+
+  responses = [
+      ['/roll', random.randint(1,100)]
+  ]
+
+  for response in responses:
+      if response[0] in content:
+          await message.reply(response[1])
 
