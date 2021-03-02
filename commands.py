2f4652f41412ab4031371a1573ae2f7578cd56c8
Tim Macdonald <tsmacdonald@gmail.com>
2021-03-02 13:30:55 -0600

    use regexes to have two problems

--- commands.py
+++ commands.py
@@ -4,6 +4,13 @@ import re
 
 bot = commands.Bot(command_prefix="/")
 
+INSTRUCTIONS = {
+    "+": (lambda x, y: x + y),
+    "-": (lambda x, y: x - y),
+    "*": (lambda x, y: x * y)
+}
+
+DICE_RE = r"(\d+)d(\d+)([*+\-]\d+)?"
 
 @bot.command()
 async def puppet(ctx, channel_name, msg):
@@ -15,45 +22,35 @@ async def puppet(ctx, channel_name, msg):
 def nice_dice(dice):
     return dice.strip().lower()
 
+def do_the_thing(dice):
+    rolls = []
+    result = 0
+    for (dice_count, face_count, math) in re.findall(DICE_RE, dice):
+        sub_result = 0
+        for _ in range(int(dice_count)):
+            roll = random.randint(1, int(face_count))
+            rolls.append(str(roll))
+            sub_result += roll
+        if math != "":
+            sub_result = INSTRUCTIONS[math[0]](sub_result, int(math[1:]))
+        result += sub_result
+    return rolls, result
+
 @bot.command()
 async def roll(ctx, *, arg=None):
     if arg is None:
         await ctx.send(str(random.randint(1, 100)))
         return
-    
-    dice = arg.strip().lower()
-    dice_count = 1
-    result = 0
+
     error_message = "Something went wrong. It's probably Ryan's fault."
     instruction = ""
-    rolls = []
-    instructions = {
-        "+": (lambda x, y: x + y),
-        "-": (lambda x, y: x - y),
-        "*": (lambda x, y: x * y)
-    }
 
-    dice_list = re.split("([d\+\*\-])", dice)
-
+    rolls, result = [], 0
     try:
-        for x in dice_list:
-            el = x.strip()
-            if not el.isdigit():
-                instruction = el
-                continue
-
-            if instruction == "":
-                dice_count = int(el)
-                continue
-
-            if instruction == "d":
-                for x in range(dice_count):
-                    roll = random.randint(1, int(el))
-                    rolls.append(str(roll))
-                    result += roll
-
-            if instruction in ["+", "-", "*"]:
-                result = instructions[instruction](result, int(el))
+        nice_arg = nice_dice(arg)
+        if not re.search("(" + DICE_RE + r"\s+)+", nice_arg):
+            raise Exception("haha")
+        rolls, result = do_the_thing(nice_arg)
     except Exception as e: #yolo
         await ctx.send(error_message)
         print(e)
