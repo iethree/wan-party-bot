6468fd9b0ecf1aa517614c4b84495e80030b5297
Ryan Laurie <iethree@gmail.com>
2022-10-11 19:11:00 -0600

    the master

--- dick.py
+++ dick.py
@@ -5,6 +5,7 @@ dickenses = open('./data/bleak-house.txt', encoding='utf8').read().split('\n\n')
 willies = open('./data/willy.txt', encoding='utf8').read().split('\n\n')
 summas = open('./data/summa.txt', encoding='utf8').read().split('\n\n')
 janes = open('./data/jane.txt', encoding='utf8').read().split('\n\n')
+yoda = open('./data/star-wars-quotes.txt', encoding='utf8').read().split('\n\n')
 
 
 dudes = {
@@ -21,5 +22,5 @@ def get_random_quote(name):
 
   while (len(choice) < 100 or len(choice) > 800):
     choice = random.choice(pool)
-  
+
   return choice
