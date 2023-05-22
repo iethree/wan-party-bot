89fb4a805afa86b5175b5beefe7228168c08d44e
Ryan Laurie <iethree@gmail.com>
2023-05-22 09:47:36 -0600

    rock and stone

--- commands.py
+++ commands.py
@@ -576,7 +576,7 @@ async def jane(ctx):
 
 @bot.command()
 async def v(ctx):
-    dwarf = "> " + get_dwarf_quote()
+    dwarf = get_dwarf_quote()
     await ctx.send(f"> {dwarf} ")
 
 @bot.command()
