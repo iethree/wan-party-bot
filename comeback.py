6fe0943bf2d55837eb90113dd020baacaadae1c4
Ryan Laurie <iethree@gmail.com>
2024-02-20 17:27:20 -0700

    multiple personality disorder

--- comeback.py
+++ comeback.py
@@ -3,6 +3,23 @@ from blacklist import is_blacklisted_channel
 from openai import OpenAI
 ai_client = OpenAI()
 
+personalities = [
+    "sarcastic wise-cracking stand up comedian",
+    "irascible grumpy pirate who has run out of rum",
+    "sassy, no-nonsense, tell-it-like-it-is friend",
+    "horny, flirty, middle schooler",
+    "paranoid conspiracy theorist",
+    "pretentious, snobby, wine critic",
+    "socially awkward genius",
+    "1950s gangster who talks like a dame",
+    "southern belle with a dirty mouth",
+    "granola-eating, tree-hugging, nixon-hating 1960s hippie",
+    "politician who will say anything to get elected"
+]
+
+def get_personality():
+    return random.choice(personalities)
+
 def get_comeback(msg):
     comebacks = [
         "that's what she said 😏",
@@ -16,10 +33,12 @@ def get_comeback(msg):
     return random.choice(comebacks)
 
 def get_ai_comeback(msg):
+    personality = get_personality()
+    print("answering as a " + personality)
     completion = ai_client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Your name is WanBot and you are a sarcastic wise-cracking stand up comedian."},
+            {"role": "system", "content": "Your name is WanBot and you are a " + personality},
             {"role": "user", "content": "write a short comeback to " + msg }
         ]
     )
