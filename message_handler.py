8e9c77cce0c67fe8d81dc089cec37b305eff3a56
Ryan Laurie <iethree@gmail.com>
2026-04-25 10:25:01 -0600

    test

--- message_handler.py
+++ message_handler.py
@@ -147,5 +147,5 @@ async def respond_to(client, message):
         if re_matches or str_matches:
             await message.channel.send(response)
 
-    if sometimes(0.05):
+    if sometimes(1):
         await appropriate_reaction(message)
